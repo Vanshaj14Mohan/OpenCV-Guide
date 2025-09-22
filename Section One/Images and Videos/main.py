85eb0141b2a48d23f59229b15d1a7caeb530db9a
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-23 00:11:09 +0530

    Files

--- Section One/Images and Videos/main.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-img = cv2.imread("Section One\Images and Videos\Cat_image.jpg")
-
-cv2.imshow("Cat", img)
-cv2.waitKey(0)
---
+++ Section One/main.py
@@ -0,0 +1,6 @@
+import cv2
+
+img = cv2.imread("Section One\Cat_image.jpg")
+
+cv2.imshow("Cat", img)
+cv2.waitKey(0) 
