90424549a57910e9e9241453f94ec85db2853b2c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-23 01:45:34 +0530

    Files

---
+++ Section One/main2.py
@@ -0,0 +1,12 @@
+import cv2
+
+capture = cv2.VideoCapture(r"Section One\SmallCat_Video.mp4")
+
+while True:
+    isTrue, frame = capture.read()
+    cv2.imshow("Video", frame)
+    if cv2.waitKey(20) & 0xFF==ord("d"):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
