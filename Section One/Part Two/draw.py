2896485cd819b429db98d0c0649ef1d9599ce895
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-24 20:24:37 +0530

    Files

--- Section One/Part Two/draw.py
+++ Section One/Part Two/draw.py
@@ -16,6 +16,10 @@ cv2.imshow("Color Image", blank_image)
 blank_image[200:300, 300:400] = 0,0,255 # Red Color
 cv2.imshow("Portion Color Image", blank_image)
 
+# 3: Drawing a Reactangle
+cv2.rectangle(blank_image, (0,0), (250, 350), (250,0,0), 2) # Blue Rectangle, cv2.FILLED for filled rectangle
+cv2.imshow("Rectangle", blank_image)
+
 # cv2.imshow("Cat Image", img)
 # cv2.imshow("Blank Image", blank_image)
 cv2.waitKey(0)
