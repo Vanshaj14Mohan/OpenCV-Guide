f5d0e97e78e678035a07bce4a3dc8a325027e6ff
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-24 23:24:21 +0530

    Files

--- Section One/Part Two/draw.py
+++ Section One/Part Two/draw.py
@@ -5,20 +5,24 @@ img = cv2.imread("Section One\Part Two\Cat_image.jpg")
 
 # Creating a blank image
 blank_image = np.zeros((650, 500, 3), dtype='uint8')
-cv2.imshow("Blank Image", blank_image)
+# cv2.imshow("Blank Image", blank_image)
 
 # 1:Giving the blank image a color (Blue, Green, Red)
 # blank_image[:] = 255, 0, 0 # Blue Color
 blank_image[:] = 0, 255, 0 # Green Color
-cv2.imshow("Color Image", blank_image)
+# cv2.imshow("Color Image", blank_image)
 
 # 2: Drawing a certain portion of the image
 blank_image[200:300, 300:400] = 0,0,255 # Red Color
-cv2.imshow("Portion Color Image", blank_image)
+# cv2.imshow("Portion Color Image", blank_image)
 
 # 3: Drawing a Reactangle
-cv2.rectangle(blank_image, (0,0), (250, 350), (250,0,0), 2) # Blue Rectangle, cv2.FILLED for filled rectangle
+cv2.rectangle(blank_image, (0,0), (250, 350), (250,0,0), 2) # Blue Rectangle, cv2.FILLED for filled rectangle instead of thickeness
 cv2.imshow("Rectangle", blank_image)
+
+# 4: Drawing a Circle
+cv2.circle(blank_image, (400,100), 30, (255, 0, 0), -1) # Blue Circle, -1 for filled circle
+cv2.imshow("Circle", blank_image)
 
 # cv2.imshow("Cat Image", img)
 # cv2.imshow("Blank Image", blank_image)
