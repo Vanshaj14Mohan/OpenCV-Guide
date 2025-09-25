8bcc88b013c994cf8d0baf2d871e1add6be98cba
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-25 23:38:46 +0530

    Files

--- Section One/Part Two/func.py
+++ Section One/Part Two/func.py
@@ -3,6 +3,6 @@ import cv2
 
 img = cv2.imread("Section One\Part Two\Cat_image.jpg")
 
-cv2.imshow("Cat Image", img)
+cv2.imshow("Cat Image", img) # Displaying the image in a window
 cv2.waitKey(0)
 cv2.destroyAllWindows()
