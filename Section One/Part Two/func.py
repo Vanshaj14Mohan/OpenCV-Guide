5f60233ad8be9a1a3b95ac9713f4abaf47351564
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-26 01:50:56 +0530

    Files

--- Section One/Part Two/func.py
+++ Section One/Part Two/func.py
@@ -5,6 +5,13 @@ img = cv2.imread("Section One\Part Two\Cat_image.jpg")
 
 cv2.imshow("Cat Image", img) 
 
-# Display the image until a key is pressed
+#1: Converting the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image", gray)
+
+#2: Blurring the image
+blur = cv2.GaussianBlur(img, (7,7), cv2.BORDER_DEFAULT)
+cv2.imshow("Blur Image", blur)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
