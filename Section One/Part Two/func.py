8c3992fe209e691ad3b1140fad0b1321cc8968f9
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-25 23:39:14 +0530

    Files

--- Section One/Part Two/func.py
+++ Section One/Part Two/func.py
@@ -4,5 +4,7 @@ import cv2
 img = cv2.imread("Section One\Part Two\Cat_image.jpg")
 
 cv2.imshow("Cat Image", img) 
+
+# Display the image until a key is pressed
 cv2.waitKey(0)
 cv2.destroyAllWindows()
