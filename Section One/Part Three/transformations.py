a856b0d65d0af88f9c62cb06c270071f3628ac61
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-26 23:51:37 +0530

    Files

--- Section One/Part Three/transformations.py
+++ Section One/Part Three/transformations.py
@@ -1,1 +1,9 @@
 # So in this section we are gonna cover basic transformations of images.
+import cv2
+
+img = cv2.imread("Section One\Part Three\Cat_image.jpg")
+
+cv2.imshow("Cat Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
