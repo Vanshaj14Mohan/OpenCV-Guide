f11b1dfdb3283eb915437dc2b4cb5ba041607a2c
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-29 23:29:12 +0530

    Files

--- Section One/Part Three/contours.py
+++ Section One/Part Three/contours.py
@@ -2,3 +2,12 @@
 # Contours can be explained simply as a curve joining all the continuous points (along the boundary), 
 # having same color or intensity.
 # Contours are a useful tool for shape analysis and object detection and recognition.
+
+import cv2
+
+img = cv2.imread("Section One\Part Three\Cat_image.jpg")
+
+cv2.imshow("Original Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
