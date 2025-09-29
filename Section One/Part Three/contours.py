ff72bcbb2406d1dcb6e3cf6a3828154fd23ca71d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-09-29 23:26:38 +0530

    Files

---
+++ Section One/Part Three/contours.py
@@ -0,0 +1,4 @@
+# In this part we will learn how to find contours in an image using OpenCV.
+# Contours can be explained simply as a curve joining all the continuous points (along the boundary), 
+# having same color or intensity.
+# Contours are a useful tool for shape analysis and object detection and recognition.
