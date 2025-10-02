b16823bbb710c9ad4906a1195956de7d7c699da0
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-02 15:50:05 +0530

    Files

--- Section One/Part One/image_code.py
+++ Section One/Part One/image_code.py
@@ -3,5 +3,6 @@ import cv2
 
 img = cv2.imread("Section One\Part One\Cat_image.jpg")
 
-cv2.imshow("Cat", img)
-cv2.waitKey(0) 
+cv2.imshow("Cat", img) # Displaying the image in a window
+cv2.waitKey(0) # Waits indefinitely until a key is pressed
+cv2.destroyAllWindows() # Closes all the windows opened by OpenCV
