89ab67b79e45ca19576a41b05c2bcc444123e94f
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-04 23:43:33 +0530

    Files

--- Section Two/spaces.py
+++ Section Two/spaces.py
@@ -1,5 +1,16 @@
 # In this part we will learn how to switch between color spaces in OpenCV, A color spaces is basically a space of colors
 # Or we can say a system of representing an array of pixel colors.
+# Some of the most common color spaces are: BGR, RGB, HSV, LAB, Grayscale etc.
+# Each color space has its own advantages and disadvantages, and is used for different purposes in image processing and computer vision tasks.
+
+import cv2
+
+img = cv2.imread("Section Two\Cat_Image.jpg")
+
+cv2.imshow("Original Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
