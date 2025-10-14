867eebd4d1b46c5770c64e4dd3d75c1ae166aa6d
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-14 23:42:54 +0530

    Files

--- Section Two/Part Two/smoothing.py
+++ Section Two/Part Two/smoothing.py
@@ -9,6 +9,11 @@ img = cv2.imread("Section Two\Part Two\BigCat_Image.jpg")
 
 cv2.imshow("Original Image", img)
 
+#1: Average Blurring
+# It takes the average of all the pixels under the kernel area and replaces the central element with this average.
+average = cv2.blur(img, (3,3)) # (3,3) is the kernel size
+cv2.imshow("Average Blurring", average)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
