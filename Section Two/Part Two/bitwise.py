d10398b44eb432daf76ecee10b96b74863f0a4be
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-20 19:22:55 +0530

    Files

---
+++ Section Two/Part Two/bitwise.py
@@ -0,0 +1,4 @@
+# In this we will learn about bitwise operations using OpenCV.
+# Bitwise operations are used to manipulate binary images. Especially useful in masking operations.
+# At a very high level, these operations work on the binary representation of the pixel values. that is 0s and 1s.
+# There are four types of bitwise operations: AND, OR, NOT, XOR.
