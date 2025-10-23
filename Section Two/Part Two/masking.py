520a5c661661ed56d58901a4e237cb4aad14ab04
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-23 23:58:30 +0530

    Files

--- Section Two/Part Two/masking.py
+++ Section Two/Part Two/masking.py
@@ -12,8 +12,22 @@ cv2.imshow("Original Image", img)
 blank = np.zeros(img.shape[:2], dtype="uint8") # Creating a blank mask with same height and width as original image
 cv2.imshow("Blank Image", blank)
 
-mask = cv2.circle(blank, (img.shape[1]//2 + 25, img.shape[0]//2 + 40), 100, 255, -1) # Creating a circular mask
+# Creating a circular mask
+mask = cv2.circle(blank, (img.shape[1]//2 + 35, img.shape[0]//2), 100, 255, -1) # Creating a circular mask
 cv2.imshow("Mask", mask)
+
+#Extra code------
+circle_mask = cv2.circle(blank.copy(), (img.shape[1]//2 + 35, img.shape[0]//2), 100, 255, -1) # Creating a circular mask
+cv2.imshow("Mask", circle_mask)
+
+rectangle = cv2.rectangle(blank.copy(), (40,40), (380,380), 255, -1)
+
+weird_mask = cv2.bitwise_xor(circle_mask, rectangle)
+cv2.imshow("Wierd Mask", weird_mask)
+# ---------------- Extra Code ends here
+
+# Creating a rectangular mask
+rect_mask = cv2.rectangle(blank, (img.shape[1]//2, img.shape[0]//2),(img.shape[1]//2 + 90, img.shape[0]//2 + 90) ,100, 255, -1) # Creating a circular mask
 
 # Now creating a masked image using bitwise AND operation
 masked_img = cv2.bitwise_and(img, img, mask=mask) # Applying the mask to the original image
