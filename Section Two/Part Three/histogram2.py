97b45024363870315beb57bc252425567ff4d434
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-24 20:02:46 +0530

    Part Three

--- Section Two/Part Three/histogram2.py
+++ Section Two/Part Three/histogram2.py
@@ -11,20 +11,26 @@ cv2.imshow("Original Image", img)
 blank = np.zeros(img.shape[:2], dtype="uint8") # Creating a blank mask with same height and width as original image
 
 # Creating a circular mask
-circle= cv2.circle(blank, (img.shape[1]//2 + 35, img.shape[0]//2), 100, 255, -1) # Creating a circular mask
-mask = cv2.bitwise_and(img, img, mask=circle) # Applying the mask to the grayscale image
-cv2.imshow("Masked Color Image", mask)
+mask= cv2.circle(blank, (img.shape[1]//2 + 35, img.shape[0]//2), 100, 255, -1) # Creating a circular mask, gave it name mask to avoid confusion with grayscale mask
+masked = cv2.bitwise_and(img, img, mask=mask) # Applying the mask to the grayscale image
+cv2.imshow("Masked Color Image", masked)
 
 # Computing color histogram for each channel (BGR)
 colors = ("b", "g", "r") # Defining color channels using tuples created by OpenCV
+
+plt.figure()
+plt.title("Grayscale Histogram")
+plt.xlabel("Bins")
+plt.ylabel("Number of Pixels")
 for i, col in enumerate(colors):
-    hist = cv2.calcHist([img], [i], None, [256], [0,256]) # Calculating histogram for each channel
+    hist = cv2.calcHist([img], [i], mask, [256], [0,256]) # Calculating histogram for each channel
     plt.plot(hist, color=col) # Plotting histogram with respective color
     plt.xlim([0,256])
     plt.title("Color Histogram") 
-    plt.xlabel("Bins")
-    plt.ylabel("Number of Pixels")
-    plt.show()
+    # plt.xlabel("Bins")
+    # plt.ylabel("Number of Pixels")
+
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
