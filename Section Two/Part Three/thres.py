389e9beb500cdd38730e288e2b35636ecc4542fa
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-10-29 23:56:48 +0530

    Part Three

--- Section Two/Part Three/thres.py
+++ Section Two/Part Three/thres.py
@@ -1,2 +1,11 @@
 # In this part we will learn about thresholding in OpenCV, 
 # Thresholding is basically a binary realisation of an image.
+
+import cv2
+
+img = cv2.imread("Section Two\Part Three\BigCat_Image.jpg")
+
+cv2.imshow("Original Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
