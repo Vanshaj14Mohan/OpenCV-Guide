14ae700c7787c6d29141ed6ef074315895bf557b
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-11-03 20:17:49 +0530

    Part Three

--- Section Two/Part Three/thres.py
+++ Section Two/Part Three/thres.py
@@ -1,3 +1,18 @@
 # In this part we will learn about thresholding in OpenCV, 
 # Thresholding is basically a binary realisation of an image.
+# That is we take an image and convert it into a binary image, pixels are basically 0(Black), 255(White).
 
+import cv2 
+
+img = cv2.imread("Section Two\Part Three\BigCat_Image.jpg")
+
+cv2.imshow("Original Image", img)
+
+# Starting with simple thresholding, converting bgr into grayscale first
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("GrayScale Image", gray)
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
