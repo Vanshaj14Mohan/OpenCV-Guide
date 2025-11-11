84d6ef16e7cfd1afab13515444312fd2af601130
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-11-11 23:20:14 +0530

    Part Three

--- Section Two/Part Three/gradients.py
+++ Section Two/Part Three/gradients.py
@@ -2,3 +2,11 @@
 # Gradients are basically the change in intensity or color values in an image.
 # They are used to detect edges and transitions in images.
 
+import cv2
+
+img = cv2.imread("Section Two\Part Three\BigCat_Image.jpg")
+
+cv2.imshow("Original Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
