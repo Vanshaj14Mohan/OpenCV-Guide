01790b2d317c8f224b3cee9391926375b740d6ff
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-11-13 23:26:38 +0530

    Section Three

---
+++ Section Three/face_detect.py
@@ -0,0 +1,11 @@
+# In this script, we will implement a simple face detection using OpenCV's pre-trained Haar Cascade classifier.
+import cv2
+
+# Load the pre-trained Haar Cascade classifier for face detection
+
+img = cv2.imread()
+
+cv2.imshow("Original Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
