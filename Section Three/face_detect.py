2d184f26ea7796565af86b7e6f38a7d9ee750bae
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-11-16 21:55:21 +0530

    Section Three

--- Section Three/face_detect.py
+++ Section Three/face_detect.py
@@ -3,9 +3,13 @@ import cv2
 
 # Load the pre-trained Haar Cascade classifier for face detection
 
-img = cv2.imread()
+img = cv2.imread("Section Three\Photos\girl.jpg")
 
 cv2.imshow("Original Image", img)
+
+# Converting image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray Image", gray)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
