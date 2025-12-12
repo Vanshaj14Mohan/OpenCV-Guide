3ec7b13401e43c438d97a656c0050369207cab44
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-12-12 14:58:15 +0530

    Section Three

--- Section Three/Part Two/face_recog.py
+++ Section Three/Part Two/face_recog.py
@@ -1,6 +1,8 @@
-# In this we will be building a face recognition system using system using OpenCV's built-in recognizer.
+# In this we will be building a face recognition system using system using OpenCV's built-in recognizer. 
+# We will be using LBPH (Local Binary Patterns Histograms) Face Recognizer for this purpose.
 import os
 import cv2
 import numpy as np 
 
-people = ["Barack Obama", "Sundar Pichai", "Elon Musk", "Tom Cruise", "Michael Jackson"] # Number of faces we will perform operations on
+people = ["Barack Obama", "Sundar Pichai", "Elon Musk", "Tom Cruise", "Michael Jackson"] # Number of faces we will perform operations on 
+
